a4c712c3fd96a49782dc50e4265e830923e44415
Soumyadeep Biswas <soumyadeep009@gmail.com>
2024-02-06 23:32:04 +0530

    Added Even Odd Checker

---
+++ src/main/java/EvenOdd.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package main.java;
+
+/**
+ * @author sobiswas
+ *
+ */
+public class EvenOdd {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int num=8;
+		boolean isEven = num%2==0;
+		System.out.println(isEven);
+	}
+
+}
