9856835b4e6b8f4c68c18f495241931dd87b0d3c
Madhusree Sen <madhu.d.1991@gmail.com>
2024-02-06 23:01:02 +0530

    Commiting Data Type codes

---
+++ src/com/study/DataTypes.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.study;
+
+/**
+ * 
+ */
+public class DataTypes {
+
+
+	
+	public static void main(String[] args) {
+		
+		int a=2;
+		int b=10;
+		
+		int c = a+b;
+		
+		char d = 'X';
+		d++;
+		
+		System.out.println(c);
+		System.out.println(d);
+
+	}
+
+}
